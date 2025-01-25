10b8e6795246cdc2001d8e9b8e63c175fc3f2b5b
ayushmishra1704 <111661160+ayushmishra1704@users.noreply.github.com>
2025-01-26 03:14:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ elif uploaded_file is not None:
     with st.expander("Visualizations"):
         visualization_type = st.selectbox(
             "Select Visualization Type",
-            [None,"Pairplot", "Scatter Plot", "Histogram", "Heatmap" , "Bar Plot","Line Plot","Violin Plot","Time-Series Plot"]
+            [None, "Pairplot", "Scatter Plot", "Histogram", "Heatmap", "Bar Plot", "Line Plot", "Violin Plot", "Time-Series Plot"]
         )
         if visualization_type == None:
             st.text("Choose a plot")
@@ -38,7 +38,7 @@ elif uploaded_file is not None:
             eda.display_histogram(data)
         elif visualization_type == "Heatmap":
             st.subheader("Heatmap Settings")
-            correlation_method = st.selectbox("Select Correlation Method", ["pearson", "spearman", "kendall"], index=0,)
+            correlation_method = st.selectbox("Select Correlation Method", ["pearson", "spearman", "kendall"], index=0)
             eda.display_heatmap(data, correlation_method)
         elif visualization_type == "Bar Plot":
             eda.display_barplot(data)
@@ -52,15 +52,15 @@ elif uploaded_file is not None:
     with st.expander("Outlier Detection"):
         st.subheader("Outlier Detection Techniques")
         # Get the selected method from the dropdown
-        detection_method = st.selectbox("Select Outlier Detection Technique", ["Outlier Detection: Box Plot", "Outlier Detection: Z-Score", "Outlier Detection: IQR Method", "Outlier Detection: Isolation Forest"],index=0)
+        detection_method = st.selectbox("Select Outlier Detection Technique", ["Outlier Detection: Box Plot", "Outlier Detection: Z-Score", "Outlier Detection: IQR Method", "Outlier Detection: Isolation Forest"], index=0)
         # Call the outlier detection function with the selected method
         eda.outlier_detection(data, detection_method)
 
     with st.expander("Feature Engineering"):
         st.subheader("Feature Engineering Techniques")
-        feature_eng_method = st.selectbox("Select Feature Engineering Technique",[
-            "Filter-Based Techniques: Correlation", 
-            "Wrapper-Based Techniques: Recursive Feature Elimination", 
+        feature_eng_method = st.selectbox("Select Feature Engineering Technique", [
+            "Filter-Based Techniques: Correlation",
+            "Wrapper-Based Techniques: Recursive Feature Elimination",
             "Feature Extraction: PCA"
         ],
         index=0
@@ -69,11 +69,11 @@ elif uploaded_file is not None:
         if feature_eng_method == "Filter-Based Techniques: Correlation":
             st.subheader("Filter-Based Techniques: Correlation")
             threshold = st.slider(
-            "Set Correlation Threshold", 
-            min_value=0.0, 
-            max_value=1.0, 
-            value=0.5, 
-            step=0.01
+                "Set Correlation Threshold",
+                min_value=0.0,
+                max_value=1.0,
+                value=0.5,
+                step=0.01
             )
             # Use filter_based_correlation function from preprocessing.py
             preprocessing.filter_based_correlation(data, threshold)
@@ -81,14 +81,14 @@ elif uploaded_file is not None:
         elif feature_eng_method == "Wrapper-Based Techniques: Recursive Feature Elimination":
             st.subheader("Wrapper-Based Techniques: Recursive Feature Elimination")
             target_column = st.selectbox(
-            "Select Target Column", 
-            data.select_dtypes(include=['number']).columns
+                "Select Target Column",
+                data.select_dtypes(include=['number']).columns
             )
             num_features = st.slider(
-            "Number of Features to Select", 
-            min_value=1, 
-            max_value=len(data.columns) - 1, 
-            value=5
+                "Number of Features to Select",
+                min_value=1,
+                max_value=len(data.columns) - 1,
+                value=5
             )
             # Use wrapper_based_rfe function from preprocessing.py
             preprocessing.wrapper_based_rfe(data, target_column, num_features)
@@ -96,14 +96,13 @@ elif uploaded_file is not None:
         elif feature_eng_method == "Feature Extraction: PCA":
             st.subheader("Feature Extraction: PCA")
             n_components = st.slider(
-            "Number of Principal Components", 
-            min_value=1, 
-            max_value=min(len(data.columns), len(data)), 
-            value=2
+                "Number of Principal Components",
+                min_value=1,
+                max_value=min(len(data.columns), len(data)),
+                value=2
             )
             # Use feature_extraction_pca function from preprocessing.py
             preprocessing.feature_extraction_pca(data, n_components)
 
         else:
             st.text("Select a feature engineering technique")
-
